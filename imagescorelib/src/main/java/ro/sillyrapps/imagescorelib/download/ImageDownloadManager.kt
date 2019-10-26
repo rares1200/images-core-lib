3eca59f76f73c76e2974ddb43e228aa7a9abaea9
rares1200 <rares1200@gmail.com>
2019-10-26 18:15:15 +0200

    Added support for folder on download from url

--- imagescorelib/src/main/java/ro/sillyrapps/imagescorelib/download/ImageDownloadManager.kt
+++ imagescorelib/src/main/java/ro/sillyrapps/imagescorelib/download/ImageDownloadManager.kt
@@ -4,13 +4,19 @@ import android.app.Activity
 import android.app.DownloadManager
 import android.content.Context
 import android.net.Uri
+import android.os.Environment
+import java.util.*
 
 object ImageDownloadManager {
 
-    fun downloadFromUrl(activity: Activity, url: String) {
+    fun downloadFromUrl(activity: Activity, folderName: String, url: String) {
+        val path = "${Environment.getExternalStorageDirectory()}/$folderName"
         val downloadManager = activity.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         val uri = Uri.parse(url)
         val request = DownloadManager.Request(uri)
+        val cal = Calendar.getInstance()
+        val name = "image_${cal.timeInMillis}.jpg"
+        request.setDestinationInExternalPublicDir(path, name)
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         downloadManager.enqueue(request)
     }
